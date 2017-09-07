53cbb70a1289c3c65b6f7cc8eac95a4b9a5153cd
traveldevel <31585321+traveldevel@users.noreply.github.com>
2017-09-08 01:11:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,41 @@ if(!appEnv.isLocal){
 var landscapeName = process.env.landscapeName;
 var tenantName = process.env.tenantName;
 
+var tenantNameMongoName = tenantName + "_raw_data";
+
+// mongo connect and create missing collections
+var mongoServiceName = "mongo_" + landscapeName;
+var mongoService = appEnv.getService(mongoServiceName);
+var mongoCredentials = appEnv.getServiceCreds(mongoServiceName);
+var mongoUrl = mongoCredentials.uri;
+var mongoClient = require('mongodb');
+var colections = [];
+
+console.log(mongoServiceName + " found in VCAP_SERVICES : ")
+console.log(mongoService);
+
+mongoClient.connect(mongoUrl, function(err, mongoDb) {
+    
+    console.log("Connected to mongo...");
+   
+    colections = mongoDb.collections();
+    
+    var exists = false;
+    if(collections !== undefined && collections.length > 0){
+        collections.map(c => c.s.name).includes(tenantNameMongoName);
+    }
+
+    if (!exists) {
+        mongoDb.createCollection(tenantNameMongoName, function(err, res) {
+            if (err) throw err;
+            console.log("Collection '" + tenantNameMongoName + "' created!");
+            colections = mongoDb.collections();
+            mongoDb.close();
+        });
+    }
+   
+});
+
 // new express app
 var app = express();
 
@@ -35,7 +70,21 @@ app.get('/ingest/:deviceId', function (req, res) {
     res.send(req.params);
 });
 
+// ingest respond back
+app.get('/', function (req, res) {
+
+    mongoClient.connect(mongoUrl, function(err, mongoDb) {
+        
+        console.log("Connected to mongo");
+       
+        var collections = mongoDb.collections();
+        mongoDb.close();
+
+        res.send(collections);
+    });
+});
+
 // app listen
 app.listen(8080, function () {
-    console.log('App listening on http://' + process.env.IP + ':' + process.env.PORT);
+    console.log('REST API listening on ' + appEnv.url + ':' + process.env.PORT);
 });
